84fd95e2d6f6a407ddc223d1b85611656853f3d8
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-11 12:59:20 +1100

    Persistent Bugger

---
+++ cool-challenges-on-codewars/javascript/Persistent bugger.js
@@ -0,0 +1,64 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example:
+
+//  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+//                        // and 4 has only one digit
+
+//  persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+//                         // 1*2*6 = 12, and finally 1*2 = 2
+
+//  persistence(4) === 0 // because 4 is already a one-digit number
+
+
+//    _____           _           _     _                 
+//    / ____|         | |         | |   (_)                
+//   | (___     ___   | |  _   _  | |_   _    ___    _ __  
+//    \___ \   / _ \  | | | | | | | __| | |  / _ \  | '_ \ 
+//    ____) | | (_) | | | | |_| | | |_  | | | (_) | | | | |
+//   |_____/   \___/  |_|  \__,_|  \__| |_|  \___/  |_| |_|
+
+
+function reduceNum (num){
+    array = num.toString().split('');
+    newNumber = 1
+    array.forEach(element => {
+        newNumber *= element; 
+    }); 
+    return newNumber
+}
+
+function digits(num){
+    array = num.toString().split('');
+    return array.length
+}
+
+
+function persistence(num) {
+    counter = 0;
+    while (digits(num)>1){
+        counter +=1;
+        num = reduceNum(num);
+    };
+    return counter;
+}
+
+document.write(persistence(999));
+// => 4
+
+
+// I liked this solution:
+
+// function persistence(num) {
+//     var times = 0;
+    
+//     num = num.toString();
+    
+//     while (num.length > 1) {
+//       times++;
+//       num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+//     }
+    
+//     return times;
+//  }
+
