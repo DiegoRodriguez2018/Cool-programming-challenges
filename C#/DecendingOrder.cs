06b3d6d0ce1ec90feb2d7263dbfcd093a5e4a218
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-17 18:10:40 +1100

    C# DecendingOrders

---
+++ C#/DecendingOrder.cs
@@ -0,0 +1,56 @@
+// Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 21445 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 1254859723 Output: 9875543221
+
+
+//    _____           _           _     _                 
+//    / ____|         | |         | |   (_)                
+//   | (___     ___   | |  _   _  | |_   _    ___    _ __  
+//    \___ \   / _ \  | | | | | | | __| | |  / _ \  | '_ \ 
+//    ____) | | (_) | | | | |_| | | |_  | | | (_) | | | | |
+//   |_____/   \___/  |_|  \__,_|  \__| |_|  \___/  |_| |_|
+
+
+
+using System;
+
+public static class Kata
+{
+  static void Main(string[] args){
+    //Test
+    DescendingOrder(45621);
+    // Should return 65421
+  }
+
+  public static int DescendingOrder(int num)
+  {
+    // Bust a move right here
+    string str = num.ToString();
+    char[] charArr = str.ToCharArray();
+    Array.Sort(charArr);
+    Array.Reverse(charArr);
+    string str2 = new String(charArr);
+    int result = int.Parse(str2);
+    return result;
+  }
+  
+}
+
+
+// Favourite solution from Codewars:
+
+// using System;
+// using System.Linq;
+
+// public static class Kata
+// {
+//   public static int DescendingOrder(int num)
+//   {
+//     return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
+//   }
+// }
---
+++ C#/README.md
@@ -0,0 +1,8 @@
+To compile and run C# programs in a Linux Distribution we need Mono, which is an open source implementation of Microsoft's .NET Framework:
+https://www.mono-project.com/
+
+To compile run 
+`csc <your-program>.cs`
+
+And to execute run:
+`mono <your-program>.exe`
